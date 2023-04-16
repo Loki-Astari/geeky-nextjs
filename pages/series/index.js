1b155d8fe6bd5edf5d1301d9636909fd81d297f8
Loki Astari <Loki.Astari@gmail.com>
2023-04-16 01:41:19 -0700

    Define page

--- pages/series/index.js
+++ pages/series/index.js
@@ -5,10 +5,11 @@ import { humanize, markdownify } from "@lib/utils/textConverter";
 import Link from "next/link";
 const { blog_folder } = config.settings;
 import { getSinglePage } from "@lib/contentParser";
+import { sortByDate } from "@lib/utils/sortFunctions";
 import { FaFolder } from "react-icons/fa";
 import { slugify } from "@lib/utils/textConverter";
 
-const Categories = ({ categories }) => {
+const Categories = ({ series }) => {
   return (
     <Base title={"series"}>
       <section className="section pt-0">
@@ -17,22 +18,27 @@ const Categories = ({ categories }) => {
           "h1",
           "h2 mb-16 bg-theme-light dark:bg-darkmode-theme-dark py-12 text-center lg:text-[55px]"
         )}
-        <div className="container pt-12 text-center">
-          {categories.map((category, i) => (
-            <ul className="row">
-              <li
-                key={`category-${i}`}
-                className="mt-4 block lg:col-4 xl:col-3"
-              >
-                <Link
-                  href={`/categories/${category.name}`}
-                  className="flex w-full rounded-lg bg-theme-light px-4 py-4 font-bold text-dark transition hover:bg-primary hover:text-white  dark:bg-darkmode-theme-dark dark:text-darkmode-light dark:hover:bg-primary dark:hover:text-white"
-                >
-                  <FaFolder className="mr-1.5" />
-                  {humanize(category.name)} ({category.posts})
-                </Link>
-              </li>
-            </ul>
+        <div className="container pt-12">
+          {series.map((aSeries, i) => (
+            <div>
+                <div className="row">
+                  <span className="col mt-4 block flex w-full rounded-lg bg-theme-light px-4 py-4 font-bold text-dark transition hover:bg-primary hover:text-white  dark:bg-darkmode-theme-dark dark:text-darkmode-light dark:hover:bg-primary dark:hover:text-white">
+                      {humanize(aSeries.name)}
+                  </span>
+                </div>
+                {aSeries.posts.map(post => (
+                  <div className="row">
+                    <span className="col-1"/>
+                    <span className="col block mt-2 block">
+                        <Link
+                          href={`${blog_folder}/${post.slug}`}
+                        >
+                            {post.frontmatter.title}
+                        </Link>
+                    </span>
+                  </div>
+                ))}
+            </div>
           ))}
         </div>
       </section>
@@ -44,19 +50,19 @@ export default Categories;
 
 export const getStaticProps = () => {
   const posts = getSinglePage(`content/${blog_folder}`);
-  const categories = getTaxonomy(`content/${blog_folder}`, "series");
-  const categoriesWithPostsCount = categories.map((category) => {
-    const filteredPosts = posts.filter((post) =>
-      post.frontmatter.categories.map(e => slugify(e)).includes(category)
+  const series = getTaxonomy(`content/${blog_folder}`, "series");
+  const seriesWithPosts = series.map((seriesName) => {
+    const filteredPosts = sortByDate(posts).filter(post =>
+      slugify(post.frontmatter.series) == seriesName
     );
     return {
-      name: category,
-      posts: filteredPosts.length,
+      name: seriesName,
+      posts: filteredPosts,
     };
   });
   return {
     props: {
-      categories: categoriesWithPostsCount,
+      series: seriesWithPosts,
     },
   };
 };
