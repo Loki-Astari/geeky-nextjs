94af51b691bc75da3d08e4e55a7243586168474a
Loki Astari <Loki.Astari@gmail.com>
2023-04-27 23:43:38 -0700

    Fix the JSON graphs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@
 
 const nextConfig = {
   output: 'standalone',
+  rewrites: async () => {
+      return [
+        {
+            source: "/Json/",
+            destination: "/Json/",
+        }
+     ]
+  },
 };
 
 module.exports = nextConfig;
